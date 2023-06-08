5f27f553804b4faa8ff66d6a41ba5e730521994e
fernando revilla <revcast@hotmail.com>
2023-06-08 15:30:36 -0500

    vad

--- src/components/Tutoriales.jsx
+++ src/components/Tutoriales.jsx
@@ -50,8 +50,7 @@ width:100%;
 export default function Tutoriales(){
 let [data,setData]=React.useState([]);
 let isLogin=useSelector(state=>state.isloguin)
-let dispatch=useDispatch();
-const navigate=useNavigate()
+
 
 
 
